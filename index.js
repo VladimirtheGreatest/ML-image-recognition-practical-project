919628053127ded46580aed0026fcbdb73d3ea82
Vladimir <vladkodoktorov@gmail.com>
2020-10-21 14:46:17 +0100

    implementing accuracy gauge

--- index.js
+++ index.js
@@ -2,26 +2,41 @@ require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const LogisticRegression = require("./logisic-regression");
 const plot = require("node-remote-plot");
-const _ = require('lodash');
-const mnist = require('mnist-data');
+const _ = require("lodash");
+const mnist = require("mnist-data");
 
-const mnistData = mnist.training(0, 10);
+const mnistData = mnist.training(0, 1000);
 
-const features = mnistData.images.values.map(image => _.flatMap(image));
+const features = mnistData.images.values.map((image) => _.flatMap(image));
 
 //The fill() method changes all elements in an array to a static value
 //, from a start index (default 0) to an end index (default array.length). It returns the modified array.
-const encodedLabels = mnistData.labels.values.map(label => {  //replacing 0 with 1 value if the value of a number from labels matches the index of column
+const encodedLabels = mnistData.labels.values.map((label) => {
+  //replacing 0 with 1 value if the value of a number from labels matches the index of column
   const row = new Array(10).fill(0);
-  row[label] = 1
+  row[label] = 1;
   return row;
 });
 
-console.log(encodedLabels);
+const regression = new LogisticRegression(features, encodedLabels, {
+  learningRate: 1,
+  iterations: 5,
+  batchSize: 100,
+});
+
+regression.train();
+
+const testMnistData = mnist.testing(0, 100);
+const testFeatures = testMnistData.images.values.map((image) =>
+  _.flatMap(image)
+);
+const testEncodedLabels = testMnistData.labels.values.map((label) => {
+  //replacing 0 with 1 value if the value of a number from labels matches the index of column
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
 
 
-
-
-
-
-
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy', accuracy);
